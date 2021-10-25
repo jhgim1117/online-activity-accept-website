1cbe3f5ab42849ae3dc7e23a2cc989d10eb817a7
jhgim1117 <jhgim1117@gmail.com>
2021-10-25 19:36:56 +0900

    login template

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from route import member
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return member.login_get()
+    else:
+        return member.login_post()
 
 if __name__ == '__main__':
     app.run(debug=True)
