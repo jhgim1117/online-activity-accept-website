5286319d0530a4687f20144d025f1eae396c3eb5
asdf <asdf@a.com>
2021-10-26 19:33:26 +0900

    login check

--- route/member.py
+++ route/member.py
@@ -7,6 +7,11 @@ def login_get():
 
 def login_post():
     id, plain_pw = request.form["ID"], request.form["password"] #로그인할 때 아이디, 비번 get
+    hashed_pw = db.db_execute('SELECT pw FROM user WHERE nickname=?', (id,))[0]['pw']
+    if bcrypt.checkpw(plain_pw.encode('utf-8'),hashed_pw.encode('utf-8')):
+        return '<script>alert("성공");</script>'
+    else:
+        return '<script>alert("실패");</script>'
     return render_template('user/login.html')
 
 def signup_get():
@@ -18,6 +23,5 @@ def signup_post():
     generation = request.form["generation"]
     num = request.form["num"]
     hashed_pw = bcrypt.hashpw(plain_pw.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    print(hashed_pw)
     db.user_insert(name, num, generation, nickname, hashed_pw)
     return render_template('user/signup.html')
