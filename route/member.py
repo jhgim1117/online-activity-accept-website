cc21e47bd9d1e98a93e33b5b7c39872a5478490b
Brandon-M-Lee <minjae.py>
2021-10-26 19:22:39 +0900

    null

--- route/member.py
+++ route/member.py
@@ -16,10 +16,9 @@ def signup_get():
     return render_template('user/signup.html')
 
 def signup_post():
-    nickname, pw = request.form["ID"], request.form["password"] #로그인할 때 아이디, 비번 get
+    nickname = request.form["ID"] #로그인할 때 아이디, 비번 get
     name = request.form["name"]
     generation = request.form["generation"]
     num = request.form["num"]
-    print(name, num, generation, nickname, pw)
     db.user_insert(name, num, generation, nickname, pw)
-    return render_template('user/signup.html')
+    return render_template('user/login.html')
