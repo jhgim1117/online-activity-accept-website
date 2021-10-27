f95d5cfc5dbd2f99180e5d7cc13ab45508e7333f
LeeSiHwan <lhs0831lkh@naver.com>
2021-10-27 21:15:19 +0900

    정보 수정 칸 생긴거
    ㅇ

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template, request, session
 from werkzeug.utils import redirect
 from lib import db
 from route import member
-<<<<<<< HEAD
 from route.admin import user_list
-=======
->>>>>>> fd1602b38518fa6e61b046072c17946cfa2b35e6
 app = Flask(__name__)
 app.secret_key = 'asdfasdfadf'
 @app.route("/")
@@ -30,29 +27,24 @@ def signup():
     else:
         return member.signup_post()
 
-<<<<<<< HEAD
 @app.route("/admin/user_list")
 def user_list():
     return user_list.show_user_list()
-=======
-@app.route("/signout", methods=['POST'])
-def signout():
-    session.pop('user_id', None)
-    return redirect("/")
->>>>>>> fd1602b38518fa6e61b046072c17946cfa2b35e6
 
-@app.route("/configdata", methods=['get','POST'])
+@app.route("/configdata", methods=['GET','POST'])
 def configdata():
-    user_id=session['user_id']
-    #이름 학번 기수 아이디 가져오기
-    nickname = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['nickname']
-    name = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['name']
-    num = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['num']
-    generation = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['generation']
+    if request.method == 'GET':
+        user_id=session['user_id']
+        #이름 학번 기수 아이디 가져오기
+        nickname = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['nickname']
+        name = db.db_execute('SELECT name FROM user WHERE id=?', (user_id,))[0]['name']
+        num = db.db_execute('SELECT num FROM user WHERE id=?', (user_id,))[0]['num']
+        generation = db.db_execute('SELECT generation FROM user WHERE id=?', (user_id,))[0]['generation']
 
-    if 'user_id' in session: #로그인 상태일때만
-        return render_template('configdata.html', nickname=nickname, name=name, num=num, generation=generation)
-
+        if 'user_id' in session: #로그인 상태일때만
+            return render_template('user/configdata.html', nickname=nickname, name=name, num=num, generation=generation)
+    else:
+        pass
 
 
 @app.route("/admin/token")
