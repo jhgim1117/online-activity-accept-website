b12575d3b8235347dfcd1a666bb7444263ef73e7
asdf <asdf@a.com>
2021-10-28 19:26:55 +0900

    토큰 관련 문제 해결 코드 정리 링크 정리

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from lib import db
 from route import member, user_list, token
 app = Flask(__name__)
 app.secret_key = 'asdfasdfadf'
-@app.route("/")
+
+@app.route("/") ##main page
 def index():
     nickname = ''
     if 'user_id' in session:
@@ -12,30 +13,24 @@ def index():
         nickname = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['nickname']
     return render_template('index.html', nickname=nickname)
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST']) ##login page
 def login():
     if request.method == 'GET':
         return member.login_get()
     else:
         return member.login_post()
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route("/signup", methods=['GET', 'POST']) ##signup page
 def signup():
     if request.method == 'GET':
         return member.signup_get()
     else:
         return member.signup_post()
 
-
-@app.route("/admin/user_list")
-def user_list_show():
-    return user_list.show_user_list()
-
-@app.route("/signout", methods=['POST'])
+@app.route("/signout", methods=['POST']) ##signout page
 def signout():
     session.pop('user_id', None)
     return redirect("/")
-
 
 @app.route("/configdata", methods=['GET','POST'])
 def configdata():
@@ -53,6 +48,13 @@ def configdata():
     else:
         pass
 
+@app.route("/admin") ##admin/user list
+def admin():
+    return render_template('admin/index.html')
+
+@app.route("/admin/user_list") ##admin/user list
+def user_list_show():
+    return user_list.show_user_list()
 
 @app.route("/admin/token", methods=['GET', 'POST'])
 def issue_token():
@@ -60,7 +62,6 @@ def issue_token():
         return token.token_get()
     else:
         return token.token_post()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- route/token.py
+++ route/token.py
@@ -2,8 +2,6 @@ from flask import render_template, request, redirect
 from lib.db import token_insert
 import random
 
-
-
 def token_post():
     generation, num = request.form["generation"], request.form["num"]
     token_insert(generation, num, random.randint(100000, 999999))
