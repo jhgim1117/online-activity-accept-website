5c46fe1e341526de47db9f72fad2538639aef81f
asdf <asdf@a.com>
2021-10-29 20:07:56 +0900

    code 정리

--- app.py
+++ app.py
@@ -12,66 +12,23 @@ def index():
         nickname = db.db_execute('SELECT nickname FROM user WHERE id=?', (user_id,))[0]['nickname']
     return render_template('index.html', nickname=nickname)
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+@app.route("/member/<path:act>", methods=['GET', 'POST'])
+def member_act(act = None):
     if request.method == 'GET':
-        return member.login_get()
-    else:
-        return member.login_post()
-
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
-    if request.method == 'GET':
-        return member.signup_get()
-    else:
-        return member.signup_post()
-
-
-@app.route("/admin/user_list")
-def user_list_show():
-    return admin.show_user_list()
-
-@app.route("/signout", methods=['POST'])
-def signout():
-    session.pop('user_id', None)
-    session.pop('admin', None)
-    return redirect("/")
-
-
-@app.route("/configdata", methods=['GET','POST']) #회워 정보 수정페이지
-def configdata():
-    if request.method=="POST":
-        return member.configdata_post()
-
-    else:
-        pass
-
-@app.route("/configdata/update", methods=['GET','POST']) #회원 정보 수정 실시
-def configdata_update():
-    if request.method=="POST":
-        return member.db_config_update()
-
-    else:
-        pass
+        return member.treat_member(act, True)
+    elif request.method == 'POST':
+        return member.treat_member(act, False)
 
 @app.route('/admin')
 def admin_site():
-    return admin.show_admin_site()
+    return admin.admin_get()
 
-@app.route("/admin/token", methods=['GET', 'POST'])
-def issue_token():
+@app.route("/admin/<path:act>", methods=['GET', 'POST'])
+def admin_act(act = None):
     if request.method == 'GET':
-        return admin.token_get()
+        return admin.treat_admin(act, True)
     elif request.method == 'POST':
-        return admin.token_post()
-
-@app.route("/admin/token/DELETE", methods=['POST'])
-def token_delete():
-     return admin.token_delete()
-    
-@app.route('/admin/token_list')
-def token_list():
-    return admin.show_token_list()
+        return admin.treat_admin(act, False)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- route/admin.py
+++ route/admin.py
@@ -2,10 +2,22 @@ from flask import render_template, session, request, abort
 from lib import db
 import random
 
-def show_admin_site():
+def admin_get():
     if not 'admin' in session:
         return abort(403)
     return render_template('/admin/index.html')
+
+def user_list_get():
+    user_list_in_db = db.db_execute("SELECT * FROM user")
+    return render_template(
+        'admin/user_list.html',
+        user_list = user_list_in_db
+    )
+
+def token_get():
+    if not 'admin' in session:
+        return abort(403)
+    return render_template('/admin/token.html')
 
 def token_post():
     if not 'admin' in session:
@@ -15,26 +27,35 @@ def token_post():
     db.db_execute("INSERT INTO token (generation, num, token) VALUES (?, ?, ?)", (generation, num, token))
     return render_template('/admin/token.html')
 
-def token_get():
-    if not 'admin' in session:
-        return abort(403)
-    return render_template('/admin/token.html')
-
-token_list_in_db = db.db_execute("SELECT * FROM token")
-def show_token_list():
-    if not 'admin' in session:
-        return abort(403)
+def token_list_get():
+    token_list_in_db = db.db_execute("SELECT * FROM token")
     return render_template(
         'admin/token_list.html',
         user_token_list = token_list_in_db
     )
 
-user_list_in_db = db.db_execute("SELECT * FROM user")
-
-def show_user_list():
+def treat_admin(act, is_get):
+    print(act)
     if not 'admin' in session:
         return abort(403)
-    return render_template(
-        'admin/user_list.html',
-        user_list = user_list_in_db
-    )
+    if act == "user/list":
+        if is_get:
+            return user_list_get()
+        else:
+            abort(405)
+    elif act == "token":
+        if is_get:
+            return token_get()
+        else:
+            return token_post()
+    elif act == "token/delete":
+        if is_get:
+            abort(405)
+        else:
+            return token_delete()
+    elif act == "token/list":
+        if is_get:
+            return token_list_get()
+        else:
+            abort(405)
+    abort(404)
